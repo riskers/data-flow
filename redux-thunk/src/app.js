5d6971a04f86108ddd8c070e218f507c990f963a
riskers <gaoyibobobo@gmail.com>
2018-04-11 12:21:10 +0800

    add react-router

--- redux-thunk/src/app.js
+++ redux-thunk/src/app.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { HashRouter as Router, Route, NavLink } from 'react-router-dom';
 import { hot } from 'react-hot-loader'
-
 import Home from 'pages/Home'
+import Test from 'pages/Test'
+
+import './global.css'
+
+const activeStyle = {
+  color: '#d0021b',
+  textDecoration: 'underline'
+}
 
 const App = () => {
   return (
-    <Home/>
+    <Router>
+      <div>
+        <nav styleName="nav">
+          <NavLink to="/" styleName="link" exact activeStyle={activeStyle}>Home</NavLink>
+          <NavLink to="/test" styleName="link" activeStyle={activeStyle}>Test</NavLink>
+        </nav>
+
+        <Route exact path="/" component={Home} />
+        <Route path="/test" component={Test} />
+      </div>
+    </Router>
   )
 }
 
---
+++ redux-thunk/src/pages/Test/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Test = () => (
+  <div>
+    this is test
+  </div>
+)
+
+export default Test
